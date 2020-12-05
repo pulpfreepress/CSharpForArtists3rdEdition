ccb63855467df707d21e2613cb7e2f167d66f75d
Rick Miller <rick@pulpfreepress.com>
2020-12-05 11:34:36 -0500

    Added using statement for required MEF namespaces.

--- Chapter10/EngineDemoMEF/EngineDemoMEF/Program.cs
+++ Chapter10/EngineDemoMEF/EngineDemoMEF/Program.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition; // added
+using System.ComponentModel.Composition.Hosting; // added
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
