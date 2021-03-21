e78993a4dcf8ccf1c15c9a91b6e3bc041b1d3cc8
Rick Miller <rick@pulpfreepress.com>
2021-03-21 12:11:48 -0400

    Added new example.

---
+++ Chapter13/MonitorObjectPropertyChange/InfrastructureLayer/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Web.Script.Serialization;
+
+namespace InfrastructureLayer {
+	public class Person : INotifyPropertyChanged {
+
+		// Fields
+		private string _firstName;
+		private string _lastName;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public string FirstName {
+			get { return _firstName; }
+			set {
+				if (value != _firstName) {
+					_firstName = value;
+					this.NotifyPropertyChanged();
+				}
+			}
+		}
+
+		public string LastName {
+			get { return _lastName; }
+			set {
+				if (value != _lastName) {
+					_lastName = value;
+					this.NotifyPropertyChanged();
+				}
+			}
+		}
+
+
+		private void NotifyPropertyChanged([CallerMemberName] string propertyName = "") {
+			if(PropertyChanged != null) {
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+
+		public override string ToString() {
+			return FirstName + " " + LastName;
+		}
+
+		public string ToJSON() {
+			return new JavaScriptSerializer().Serialize(this).ToString();
+		}
+
+	} // end class
+} // end namespace
---
+++ Chapter13/MonitorObjectPropertyChange/PresentationLayer/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfrastructureLayer;
+using Subscriber;
+
+namespace PresentationLayer {
+	class MainApp {
+		static void Main(string[] args) {
+			Person p = new Person();
+			p.FirstName = "Rick";
+			p.LastName = "Miller";
+
+			Console.WriteLine(p.ToJSON());
+
+			Console.ReadKey();
+			
+		}
+	}
+}
